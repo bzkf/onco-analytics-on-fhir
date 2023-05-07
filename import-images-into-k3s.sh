34edb21d7703b678fb84c7c12c608811681df8c9
chgl <chgl@users.noreply.github.com>
2023-05-07 22:28:36 +0200

    fix: added script to import all images into k3s

---
+++ import-images-into-k3s.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euox pipefail
+
+if [ -z "$IMAGE_FOLDER" ]; then
+  IMAGE_FOLDER="../images/"
+fi
+
+# Check if folder exists
+if [ ! -d "$IMAGE_FOLDER" ]; then
+  echo "Error: $IMAGE_FOLDER does not exist."
+  exit 1
+fi
+
+# Iterate over all tar files in the folder
+for file in "$IMAGE_FOLDER"/*.tar; do
+  echo "Importing image from $file"
+  k3s ctr images import "$file"
+done
