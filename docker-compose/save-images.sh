787b21ef6f7481d8797f50c61fe5d928501e96e5
chgl <chgl@users.noreply.github.com>
2023-05-08 21:13:51 +0200

    fix: don't pin compose digests (#53)
    fix: anonymization to work with multiple identifier coding types
    
    fix: un-pin compose images due to missing repo names when docker-saving
    
    feat: added onkostar db connector

--- docker-compose/save-images.sh
+++ docker-compose/save-images.sh
@@ -11,14 +11,15 @@
 # Enumerate docker images to be processed.
 
 DOCKER_IMAGE_NAMES=(
-  "docker.io/tchiotludo/akhq:0.24.0@sha256:6ccf8323ae6e93a893107f857cd9f7210add3569743b2c8528c6567967cc636f"
-  "docker.io/library/postgres:15.1@sha256:10d6e725f9b2f5531617d93164f4fc85b1739e04cab62cbfbfb81ccd866513b8"
-  "ghcr.io/miracum/fhir-gateway:v3.10.9@sha256:f4bae212ee8c137c522df7610b931d648ee7a5dd237f842432ae092fc3adbd0a"
-  "ghcr.io/miracum/fhir-pseudonymizer:v2.17.0@sha256:431a7f4516c1f641966c507941b87a997441665656572a1c00c44376ef08fe24"
-  "docker.io/hapiproject/hapi:v6.4.0@sha256:638b8df98fcfc074404520700923c07d313f62c5dcad49ca569cdb8041c7cc57"
-  "docker.io/bitnami/kafka:3.4.0@sha256:f091afcc2a1db46f88dbc87839e7533adab2f3e8b95a0dbe0505a7bc8fe06262"
-  "harbor.miracum.org/streams-ume/onkoadt-to-fhir:v1.10.0@sha256:e584922155db6e8c281a9b7fc3728701d0f08f0713ebd35c9931492a761c003a"
-  "docker.io/library/traefik:v2.10.1@sha256:2a1c328e239fa9cb3891a98c4c1beee300bf467fc7add316632e82de24529b49"
+  "docker.io/tchiotludo/akhq:0.24.0"
+  "docker.io/library/postgres:15.2"
+  "ghcr.io/miracum/fhir-gateway:v3.10.9"
+  "ghcr.io/miracum/fhir-pseudonymizer:v2.17.0"
+  "docker.io/hapiproject/hapi:v6.4.4"
+  "ghcr.io/miracum/kafka-fhir-to-server:v1.2.3"
+  "docker.io/bitnami/kafka:3.4.0"
+  "harbor.miracum.org/streams-ume/onkoadt-to-fhir:v1.10.0"
+  "docker.io/library/traefik:v2.10.1"
 )
 
 # Make output variables.
