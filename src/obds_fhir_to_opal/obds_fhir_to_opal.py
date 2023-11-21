c9a93dfe97308dd178a0f32fb0891d9c64f9f627
Ziegler, Jasmin <jasmin.ziegler@uk-erlangen.de>
2023-11-21 15:51:41 +0100

    fix: improve entity grouping after review

--- src/obds_fhir_to_opal/obds_fhir_to_opal.py
+++ src/obds_fhir_to_opal/obds_fhir_to_opal.py
@@ -270,81 +270,45 @@ def lookup_condcodingcode_mapping(icd10_code):
 
 
 def group_entities(condcodingcode_mapped):
-    entity_group = 100
     condcodingcode_mapped = float(condcodingcode_mapped)
-    if condcodingcode_mapped >= 300 and condcodingcode_mapped < 315:
-        entity_group = 0  # Lippe, Mundhöhle und Rachen (C00-C14)
-    elif condcodingcode_mapped >= 315 and condcodingcode_mapped < 316:
-        entity_group = 1  # Speiseröhre (C15)
-    elif condcodingcode_mapped >= 316 and condcodingcode_mapped < 317:
-        entity_group = 2  # Magen (C16)
-    elif condcodingcode_mapped >= 318 and condcodingcode_mapped < 322:
-        entity_group = 3  # Dickdarm und Rektum (C18-C21)
-    elif condcodingcode_mapped >= 322 and condcodingcode_mapped < 323:
-        entity_group = 4  # Leber (C22)
-    elif condcodingcode_mapped >= 323 and condcodingcode_mapped < 325:
-        entity_group = 5  # Gallenblase und Gallenwege (C23-C24)
-    elif condcodingcode_mapped >= 325 and condcodingcode_mapped < 326:
-        entity_group = 6  # Bauchspeicheldrüse (C25)
-    elif condcodingcode_mapped >= 332 and condcodingcode_mapped < 333:
-        entity_group = 7  # • Kehlkopf (C32)
-    elif condcodingcode_mapped >= 333 and condcodingcode_mapped < 335:
-        entity_group = 8  # • Trachea, Bronchien und Lunge (C33-C34)
-    elif condcodingcode_mapped >= 343 and condcodingcode_mapped < 344:
-        entity_group = 9  # • Malignes Melanom der Haut (C43)
-    elif (
-        condcodingcode_mapped >= 350
-        and condcodingcode_mapped < 351
-        or condcodingcode_mapped >= 405
-        and condcodingcode_mapped < 406
-    ):
-        entity_group = 10  # • Brust (C50, D05)
-    elif (
-        condcodingcode_mapped >= 353
-        and condcodingcode_mapped < 354
-        or condcodingcode_mapped >= 406
-        and condcodingcode_mapped < 407
-    ):
-        entity_group = 11  # • Gebärmutterhals (C53, D06)
-    elif condcodingcode_mapped >= 354 and condcodingcode_mapped < 356:
-        entity_group = 12  # • Gebärmutterkörper (C54-C55)
-    elif (
-        condcodingcode_mapped >= 356
-        and condcodingcode_mapped < 357
-        or condcodingcode_mapped == 439.1
-    ):
-        entity_group = 13  # • Eierstöcke (C56, D39.1)
-    elif condcodingcode_mapped >= 361 and condcodingcode_mapped < 362:
-        entity_group = 14  # • Prostata (C61)
-    elif condcodingcode_mapped >= 362 and condcodingcode_mapped < 363:
-        entity_group = 15  # • Hoden (C62)
-    elif condcodingcode_mapped >= 364 and condcodingcode_mapped < 365:
-        entity_group = 16  # • Niere (C64)
-    elif (
-        condcodingcode_mapped >= 367
-        and condcodingcode_mapped < 368
-        or condcodingcode_mapped == 409.0
-        or condcodingcode_mapped == 441.4
-    ):
-        entity_group = 17  # • Harnblase (C67, D09.0, D41.4)
-    elif condcodingcode_mapped >= 370 and condcodingcode_mapped < 373:
-        entity_group = 18  # • Gehirn und zentrales Nervensystem (C70-C72)
-    elif condcodingcode_mapped >= 373 and condcodingcode_mapped < 374:
-        entity_group = 19  # • Schilddrüse (C73)
-    elif condcodingcode_mapped >= 381 and condcodingcode_mapped < 382:
-        entity_group = 20  # • Morbus Hodgkin (C81)
-    elif (
-        condcodingcode_mapped >= 382
-        and condcodingcode_mapped < 389
-        or condcodingcode_mapped >= 396
-        and condcodingcode_mapped < 397
-    ):
-        entity_group = 21  # • Non-Hodgkin-Lymphome (C82-C88, C96)
-    elif condcodingcode_mapped >= 390 and condcodingcode_mapped < 391:
-        entity_group = 22  # • Plasmozytom (C90)
-    elif condcodingcode_mapped >= 391 and condcodingcode_mapped < 396:
-        entity_group = 23  # • Leukämien (C91-C95)
-    return entity_group
+    ranges = [
+        (300, 315, 0),  # Lippe, Mundhöhle und Rachen (C00-C14)
+        (315, 316, 1),  # Speiseröhre (C15)
+        (316, 317, 2),  # Magen (C16)
+        (318, 322, 3),  # Dickdarm und Rektum (C18-C21)
+        (322, 323, 4),  # Leber (C22)
+        (323, 325, 5),  # Gallenblase und Gallenwege (C23-C24)
+        (325, 326, 6),  # Bauchspeicheldrüse (C25)
+        (332, 333, 7),  # Kehlkopf (C32)
+        (333, 335, 8),  # Trachea, Bronchien und Lunge (C33-C34)
+        (343, 344, 9),  # Malignes Melanom der Haut (C43)
+        (350, 351, 10),  # Brust (C50, D05)
+        (405, 406, 10),
+        (353, 354, 11),  # Gebärmutterhals (C53, D06)
+        (406, 407, 11),
+        (354, 356, 12),  # Gebärmutterkörper (C54-C55)
+        (356, 357, 13),  # Eierstöcke (C56, D39.1)
+        (439.1, 439.2, 13),
+        (361, 362, 14),  # Prostata (C61)
+        (362, 363, 15),  # Hoden (C62)
+        (364, 365, 16),  # Niere (C64)
+        (367, 368, 17),  # Harnblase (C67, D09.0, D41.4)
+        (409.0, 409.1, 17),
+        (441.4, 441.5, 17),
+        (370, 373, 18),  # Gehirn und zentrales Nervensystem (C70-C72)
+        (373, 374, 19),  # Schilddrüse (C73)
+        (381, 382, 20),  # Morbus Hodgkin (C81)
+        (382, 389, 21),  # Non-Hodgkin-Lymphome (C82-C88, C96)
+        (396, 397, 21),
+        (390, 391, 22),  # Plasmozytom (C90)
+        (391, 396, 23),  # Leukämien (C91-C95)
+    ]
+
+    for start, end, group in ranges:
+        if start <= condcodingcode_mapped < end:
+            return group
+
+    return 100
 
 
 def encode_conditions(ptl: PathlingContext, df_bundles):
