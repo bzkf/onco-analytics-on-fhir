3ca335449f026d1bdf08fb884393dfc143745150
Jasmin Ziegler <ziegler-jasmin@outlook.de>
2024-05-06 11:21:58 +0200

    megalinter fix

--- src/decompose_xmls/decompose_xmls.py
+++ src/decompose_xmls/decompose_xmls.py
@@ -10,7 +10,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     input_folder: str = "./input-obds-reports"
-    output_folder: str = "./output-obds-reports"
+    output_folder: str = "./output-obds-reports"   # RENAME THIS
     output_folder_xml: str = "./output-obds-reports/output-xmls"
     save_as_files_enabled: bool = True
     kafka_enabled: bool = False
@@ -51,7 +51,7 @@ def decompose_sammelmeldung(root: ET.Element, filename: str):
         kafka_producer = Producer(
             {"bootstrap.servers": settings.kafka_bootstrap_servers}
         )
-    # Get all "Patient" elements, save the absender to be appended to each new file
+    # Get all "Patient" elements, save the absender to be appended to each new file - IN THE END
     patients = root.findall(".//{http://www.gekid.de/namespace}Patient")
     absender = root.find("./{http://www.gekid.de/namespace}Absender")
 
@@ -75,78 +75,78 @@ def decompose_sammelmeldung(root: ET.Element, filename: str):
             continue
 
         # remove all Menge_Meldung
-        menge_meldung = patient.find("./{http://www.gekid.de/namespace}Menge_Meldung")
+        menge_meldung = patient.find('./{http://www.gekid.de/namespace}Menge_Meldung')
         if menge_meldung is not None:
             patient.remove(menge_meldung)
 
-        for meldung in menge_meldung:
-            # build Menge_Meldung group
-            meldung_id = meldung.get("Meldung_ID")
-            menge_meldung_group = ET.Element("Menge_Meldung")
-            menge_meldung_group.append(meldung)
-            # build parent tag Patient, append two children on the same level:
-            # append child Patient_Stammdaten first,
-            # append child Menge_Meldung_Group second
-            element_patient = ET.Element("Patient")
-            element_patient.append(patient_stammdaten)
-            element_patient.append(menge_meldung_group)
-            # build grandparent tag Menge_Patient and add Patient + children
-            menge_patient = ET.Element("Menge_Patient")
-            menge_patient.append(element_patient)
-            # build root element
-            meldung_root = ET.Element(root.tag, root.attrib)
-            # append absender
-            meldung_root.append(absender)
-            # append menge_patient built above
-            meldung_root.append(menge_patient)
+            for meldung in menge_meldung:
+                # build Menge_Meldung group
+                meldung_id = meldung.get("Meldung_ID")
+                menge_meldung_group = ET.Element("Menge_Meldung")
+                menge_meldung_group.append(meldung)
+                # build parent tag Patient, append two children on the same level:
+                # append child Patient_Stammdaten first,
+                # append child Menge_Meldung_Group second
+                element_patient = ET.Element("Patient")
+                element_patient.append(patient_stammdaten)
+                element_patient.append(menge_meldung_group)
+                # build grandparent tag Menge_Patient and add Patient + children
+                menge_patient = ET.Element("Menge_Patient")
+                menge_patient.append(element_patient)
+                # build root element
+                meldung_root = ET.Element(root.tag, root.attrib)
+                # append absender
+                meldung_root.append(absender)
+                # append menge_patient built above
+                meldung_root.append(menge_patient)
 
-            ET.register_namespace("", "http://www.gekid.de/namespace")
-            f = BytesIO()
-            tree = ET.ElementTree(meldung_root)
-            ET.indent(tree, "  ")
-            tree.write(
-                f, encoding="utf-8", xml_declaration=True
-            )
+                ET.register_namespace("", "http://www.gekid.de/namespace")
+                f = BytesIO()
+                tree = ET.ElementTree(meldung_root)
+                ET.indent(tree, "  ")
+                tree.write(
+                    f, encoding="utf-8", xml_declaration=True
+                )
 
-            xml_str = f.getvalue().decode()
-            # prepare json files for kafka bridge
-            result_data = {
-                "payload": {
-                    "LKR_MELDUNG": meldung_id,
-                    "XML_DATEN": xml_str,
-                    "VERSIONSNUMMER": 1,
-                    "REFERENZ_NUMMER": patient_id,
+                xml_str = f.getvalue().decode()
+                # prepare json files for kafka bridge
+                result_data = {
+                    "payload": {
+                        "LKR_MELDUNG": meldung_id,
+                        "XML_DATEN": xml_str,
+                        "VERSIONSNUMMER": 1,
+                        "REFERENZ_NUMMER": patient_id,
+                    }
                 }
-            }
 
-            if settings.save_as_files_enabled:
-                with open(
-                    f"{settings.output_folder}/"
-                    + f"patient_{patient_id}_meldung_"
-                    + f"{meldung_id}.json",
-                    "w",
-                    encoding="utf-8",
-                ) as output_file:
-                    json.dump(result_data, output_file, indent=4)
+                if settings.save_as_files_enabled:
+                    with open(
+                        f"{settings.output_folder}/"
+                        + f"patient_{patient_id}_meldung_"
+                        + f"{meldung_id}.json",
+                        "w",
+                        encoding="utf-8",
+                    ) as output_file:
+                        json.dump(result_data, output_file, indent=4)
 
-            save_xml_files(
-                    meldung_root,
-                    patient_id,
-                    meldung_id,
-                )
+                save_xml_files(
+                        meldung_root,
+                        patient_id,
+                        meldung_id,
+                    )
 
-            if kafka_producer is not None:
-                kafka_producer.poll(0)
+                if kafka_producer is not None:
+                    kafka_producer.poll(0)
 
-                # Asynchronously produce a message. The delivery report callback will
-                # be triggered from the call to poll() above, or flush() below, when the
-                # message has been successfully delivered or failed permanently.
-                kafka_producer.produce(
-                    settings.kafka_output_topic,
-                    json.dumps(result_data),
-                    callback=kafka_delivery_report,
-                    key=f"{patient_id}-{meldung_id}",
-                )
+                    # Asynchronously produce a message. The delivery report callback will
+                    # be triggered from the call to poll() above, or flush() below, when the
+                    # message has been successfully delivered or failed permanently.
+                    kafka_producer.produce(
+                        settings.kafka_output_topic,
+                        json.dumps(result_data),
+                        callback=kafka_delivery_report,
+                        key=f"{patient_id}-{meldung_id}",
+                    )
 
     if kafka_producer is not None:
         kafka_producer.flush()
